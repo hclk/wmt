6be6f9ca850c7ad79e8b869d724b7e42298095dd
hclk <harryclarkem@gmail.com>
2015-02-16 15:49:50 +0000

    Added parser to daterange

--- daterange.php
+++ daterange.php
@@ -1,28 +1,23 @@
 <?php
 	include 'gwtdata.php';
 	try {
-		$email = "example@example.com";
-		$passwd = "**************";
-		$sitestring = "example.com";
+		$email = "email@example.com";
+		$passwd = "*************";
+		$sitestring = "example";
 
 
-		$savepath = getcwd() . "/" . $sitestring . "-" . date("Ymd-His");
+		$folder = $sitestring . "-" . date("Ymd-His");
+		$savepath = getcwd() . "/" . $folder;
+		
 		$ns = 0;
-		# Dates must be in valid ISO 8601 format.
-		//$dtoday = date('o-M-D');
 		for($i = 1; $i <= 90; $i++){
 			$dates[] = array(date('o-m-d', strtotime('-' . strval($i) . ' days')),date('o-m-d', strtotime('-' . strval($i-1) . ' days')));
-			
-
-			//print(date('o-m-d', strtotime('-' . strval($i) . ' days')) . "-" . date('o-m-d', strtotime('-' . strval($i-1) . ' days')));
-			//print(PHP_EOL);
 		}
 
 		print("Working...");
 		
 		$gdata = new GWTdata();
 		if($gdata->LogIn($email, $passwd) === true){
-			//$site = "enterprise.co.uk";
 			$sites = $gdata->GetSites();
 			foreach($sites as $site){
 				if(strpos($site, $sitestring) !== FALSE){
@@ -46,6 +41,67 @@
 			}				
 		
 		}
+
+############################################
+		## Start parsing csvs ##
+############################################	
+
+	chdir($folder);
+	$files = glob("*.csv");
+	
+	foreach($files as $file){
+		preg_match("#TOP_QUERIES\-(.*?)\-.*#", $file, $sitea);
+		if(isset($sitea[1]) && strlen($sitea[1])>2){
+			$sitesa[] = $sitea[1];
+		}
+	}
+	$sites = array_unique($sitesa);
+	foreach($sites as $site){
+		print("\r\nProcessing " . $site . "\r\n");
+		$tnl = 0;
+		$output = "Site: " . $site . PHP_EOL . PHP_EOL;
+		foreach($files as $file){
+			$file = trim($file);
+			if(strpos($file, $site) !== FALSE){
+				preg_match("#.*-(.*)#", $file, $datea);
+				$date = $datea[1];
+				$date = substr($date, 0,4)."-".substr($date, 4,2)."-".substr($date, 6,2);
+				$file = fopen($file, "r");
+				if($file){
+					$nl = 0;
+					while(!feof($file)){
+						$line = fgets($file);
+						$c = explode(",", $line);
+						
+						$nl++;
+						$tnl++;
+
+						printf("\rProcessed %s lines", $tnl);
+
+						if($tnl === 1){
+							if(strpos($line, "Change") !== FALSE){
+								$extraHeadings = TRUE;
+							} else {
+								$extraHeadings = FALSE;
+							}
+							$output .= "Date,Query,Avg.position,Impressions,Clicks" . PHP_EOL;
+						}
+						
+						if($nl > 1){
+							if($extraHeadings === TRUE && isset($c[0],$c[7],$c[1],$c[3])){
+								$output .= $date . "," . trim($c[0]) . "," . trim($c[7]) . "," . trim($c[1]) . "," . trim($c[3]) . PHP_EOL;
+							} elseif (isset($c[0],$c[4],$c[1],$c[2])){
+								$output .= $date . "," . trim($c[0]) . "," . trim($c[4]) . "," . trim($c[1]) . "," . trim($c[2]) . PHP_EOL;
+							}
+						}		
+					}
+				}
+			}
+		}
+		file_put_contents($site . "-" . date("Ymd-His") . "-concat.csv", $output);
+		$output = "";
+	}
+
 	} catch (Exception $e) {
 		die($e->getMessage());
 	}
